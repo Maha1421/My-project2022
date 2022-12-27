218f20451b248c17037cc4f0e4c9d96c93fd8934
Maha <60257033+Maha1421@users.noreply.github.com>
2022-12-27 16:40:20 +0300

    Initial Commit

---
+++ Cloudkit2022/Cloudkit2022App.swift
@@ -0,0 +1,17 @@
+//
+//  Cloudkit2022App.swift
+//  Cloudkit2022
+//
+//  Created by Maha Saleh Alghamdi on 03/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Cloudkit2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cloudkit2022/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Cloudkit2022
+//
+//  Created by Maha Saleh Alghamdi on 03/06/1444 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
